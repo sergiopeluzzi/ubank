a074dcba353b3825771812f35088bc8fde9d82e6
Sergio Peluzzi <sergiopeluzzi@gmail.com>
2021-05-11 19:38:39 -0400

    first commit

---
+++ js/classes/Pessoa.js
@@ -0,0 +1,9 @@
+export default class Pessoa {
+    constructor(nome, dtNasc, sexo, tel, end) {
+        this.nome = nome
+        this.dataNascimento = dtNasc
+        this.sexo = sexo
+        this.telefone = tel
+        this.endereco = end
+    }
+}
