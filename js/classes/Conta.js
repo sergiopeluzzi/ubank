b742f7c3efaff7735bf26fcf4c98dde5291f63bc
Sergio Peluzzi <sergiopeluzzi@gmail.com>
2021-05-11 20:21:08 -0400

    third commit

--- js/classes/Conta.js
+++ js/classes/Conta.js
@@ -1,3 +1,20 @@
 export default class Conta {
-    
+    constructor(numero, titular) {
+        this.numero = numero
+        this.titular = titular
+        this.dataAbertura = Date()
+        this.saldo = 0
+    }
+
+    depositar(quantia) {
+        if (quantia > 0) {
+            this.saldo += quantia
+        }
+    }
+
+    sacar(quantia) {
+        if (quantia <= this.saldo) {
+            this.saldo -= quantia
+        }
+    }
 }
--- js/classes/Usuario.js
+++ js/classes/Usuario.js
@@ -1,3 +1,7 @@
 export default class Usuario {
-    
+    constructor(user, pass, confirmPass) {
+        this.usuario = user
+        this.senha = pass
+        this.senhaConfirmada = confirmPass
+    }
 }
